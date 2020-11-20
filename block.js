7ae9f97f91f5267ed63854165db3909013b11d5e
bradleygichuru <prz03@protonmail.com>
2020-11-21 01:59:09 +0300

    block prototype finished but untested

--- block.js
+++ block.js
@@ -1,10 +1,30 @@
-//based on merkel tree data structures 
+//based on merkel tree data structures
+const merkel = require("merkletreejs");
+const crypto = require("crypto-js");
+
 class block {
-    constructor(data,previous_blockhash,merkle_root,timestamp){
-        this.data = data;//transaction or the data in which we wish to maintain the integrity of 
-        this.previous_blockhash = previous_blockhash;
-        this.merkle_root = merkle_root; 
-        this.timestamp = timestamp;
-    }
+  constructor(blockData, previous_blockhash) {
+    this.blockData = blockData; //transaction or the data in which we wish to maintain the integrity of
+    this.previous_blockhash = previous_blockhash;
+    this.merkle_root;
+    this.timestamp; //time on instance of block generation
+    this.processedBlock = {}; //previous blockhash ,merkle_root ,block_hash,data
+    this.curBlockHash;
+  }
+  generate() {
+    
+    this.timestamp = Date.now();
+    const tree = new MerkleTree(this.blockData, crypto.SHA256);
+    this.merkle_root = tree.getHexRoot();
+    let hash = crypto.SHA256(
+      `${
+        this.blockData.toString() +
+        this.previous_blockhash +
+        this.timestamp +
+        this.merkle_root
+      }`
+    );
+    this.curBlockHash = hash.toString(crypto.enc.Hex);
+  }
 }
-module.exports = block;
+export default block;
